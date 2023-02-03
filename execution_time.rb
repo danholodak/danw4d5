271c751e4ec61621f1383bd1a01e6c994b13b8c3
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-03 14:26:40 -0500

    Create largest_contiguous_subsum_phase_two

--- execution_time.rb
+++ execution_time.rb
@@ -1,4 +1,4 @@
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 
 def my_min_phase_one(list) #time = O(n^2) space = O(1)
 
@@ -23,3 +23,34 @@ end
 
 # p my_min_phase_two(list)
 
+def largest_contiguous_subsum_phase_one(list) # time: O(n^3) space: O(n^2)
+    subsets = []
+    i = 0
+    while i < list.length 
+        j = i 
+        while j < list.length
+            if !subsets.include?(list[i..j])
+                subsets << list[i..j]
+            end
+            j += 1
+        end
+        i += 1
+    end
+    subsets.each do |subset|
+        return subset.sum if subsets.all? {|subset2| subset.sum >= subset2.sum}
+    end
+end
+
+# list = [5, 3, -7]
+# p largest_contiguous_subsum_phase_one(list) # => 8
+
+def largest_contiguous_subsum_phase_two(list)
+    largest_sum = list[0]
+
+end
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum_phase_two(list) # => 8 (from [7, -6, 7])
+
+list2 = [-5, -1, -3]
+largest_contiguous_subsum_phase_two(list2) # => -1 (from [-1])
